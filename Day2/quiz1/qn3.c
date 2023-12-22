baa6a1baa8d417ea64c24cbe5315580681a661f9
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2023-12-22 13:45:50 +0530

    Update qn3.c

--- Day2/quiz1/qn3.c
+++ Day2/quiz1/qn3.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 int main()
 {
-    char[100] str;
+    char str[100];
+    int len=0,spaces=0,actlen,sclen,extra=0;
     printf("Enter the string :");
-    scanf("%s\n",str);
-    
+    gets(str);
+    len=strlen(str);
+    for(int i=0;i<len;i++)
+    {
+        if(str[i]==' ')
+         spaces++;
+    }
+    printf("Enter Screen length :");
+    scanf("%d",&sclen);
+    actlen=len-spaces;
+    extra=sclen-actlen;
+    for(int i=0;i<len;i++)
+    {
+        if(str[i]!=' ')
+        {
+            printf("%c",str[i]);
+        }
+        else
+        {
+            int temp=extra/spaces;
+            for(int j=0;j<temp;j++)
+            {
+              printf("*");
+            }
+            extra=extra-temp;
+            spaces=spaces-1;
+        }
+    }
+    return 1;
 }

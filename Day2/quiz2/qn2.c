543e9ba935b580e2276c55ba0a85c66015ffae24
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2023-12-22 17:18:43 +0530

    Update qn2.c

--- Day2/quiz2/qn2.c
+++ Day2/quiz2/qn2.c
@@ -5,26 +5,27 @@ int main()
 	printf("Enter the string: ");
 	gets(str);
 	int len=strlen(str);
-	int num=0;
+	int num;
 	for(int i=0;i<len;i++)
 	{
+		num=0;
 		if(isalpha(str[i]))
 		{
 			temp=str[i];
-			num=0;
-		}
-		else
-		{
-			num=num*10+str[i] - 48;
-			if(isalpha(str[i+1]))
+			for(int j=i+1;j<len;j++)
 			{
+				if(isdigit(str[j]))
+				{
+					num=num*10+str[j] - 48;
+				}
+				else 
+				  break;
+			}
 			for(int k=0;k<num;k++)
 			{
-			   printf("%c",temp);
-		    }
+				printf("%c",temp);
 			}
 		}
-		printf("*");
 	}
 	return 1;
  }

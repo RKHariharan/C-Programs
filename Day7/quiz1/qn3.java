e571a7a4004c9c2470539fecd029facee3602b57
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2023-12-27 12:01:51 +0530

    Update qn3.java

--- Day7/quiz1/qn3.java
+++ Day7/quiz1/qn3.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ashwi
- */
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;

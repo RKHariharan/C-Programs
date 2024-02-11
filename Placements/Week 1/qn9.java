45d8b827e69591ba027aaeb770922d7ef7273adc
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2024-02-11 23:22:55 +0530

    Update qn9.java

--- Placements/Week 1/qn9.java
+++ Placements/Week 1/qn9.java
@@ -1,16 +1,5 @@
 package placementcode;
 import java.util.Arrays;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author sathi
- */
 public class qn9 {
     public static void main(String[] args)
     {
